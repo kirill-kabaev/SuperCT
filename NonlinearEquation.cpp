17aa9a686d3238cb1e617ad7377df64683f1a4cf
kirill-kabaev <k.kabaev94@gmail.com>
2020-10-18 23:06:52 +0300

    initial commit

---
+++ NonlinearEquation.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+#define eps 0.000001
+#define dx 0.00001
+double fx(double x) {
+    return sqrt(x+1)+x;
+}
+double dfx(double x) {
+    return (fx(x + dx) - fx(x)) / dx;
+}
+
+typedef double(*function)(double x);
+
+double solve(function fx, double x0) {
+    int start = clock();
+    double x1 = x0 - fx(x0) / dfx(x0);
+    while (fabs(x1 - x0) > eps) {
+        x0 = x1;
+        x1 = x0 - fx(x0) / dfx(x0);
+    }
+    return x1;
+}
+
+/*
+int main() {
+    double start = clock();
+
+    double x0 = 0.;
+    double solving;
+
+    solving = solve(fx, x0);
+
+
+    double end = clock();
+    double t = end - start; // / CLOCKS_PER_SEC;
+
+    cout << solving << endl<< t <<endl;
+    return 0;
+}
+*/
